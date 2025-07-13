37ba136dc644d6996c48465448c94f2580c8c15a
Scar <scardubu@gmail.com>
2025-07-13 11:58:32 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import EchocraftLanding from './components/EchocraftLanding';
+import './index.css';
+
+function App() {
+  return <EchocraftLanding />;
+}
+
+export default App;
